5020b60381b96121cffc4a54dd9d3ee8acbcd5ac
Riyaz1110 <imohamedriyaz84@gmail.com>
2026-02-28 22:08:01 +0530

    check

--- client/src/pages/admin/Login.tsx
+++ client/src/pages/admin/Login.tsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 export default function AdminLogin() {
   const [_, setLocation] = useLocation();
   const { login, user, isLoading } = useAuth();
+  console.log("USER:", user);
   
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
