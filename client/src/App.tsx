ab0c1f4c773e82847aab73d86ea647136022a986
Riyaz1110 <imohamedriyaz84@gmail.com>
2026-02-28 22:05:43 +0530

    check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,9 +18,9 @@ function Router() {
       <Route path="/" component={Home} />
       <Route path="/checkout" component={Checkout} />
       <Route path="/admin/login" component={AdminLogin} />
-      <Route path="/admin" component={AdminDashboard} />
       <Route path="/admin/products" component={AdminProducts} />
       <Route path="/admin/orders" component={AdminOrders} />
+      <Route path="/admin" component={AdminDashboard} />
       <Route component={NotFound} />
     </Switch>
   );
